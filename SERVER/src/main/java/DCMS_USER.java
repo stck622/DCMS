c97a15dfb44d8a34f74a18ed924aed6f0dd59e2d
JJ0327 <dhehdlflrhks@Naver.com>
2018-07-17 16:22:25 +0900

    벌점추가

--- SERVER/src/main/java/DCMS_USER.java
+++ SERVER/src/main/java/DCMS_USER.java
@@ -3,6 +3,12 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DCMS_USER extends Thread { // 포트 8001
     int getnum(byte data[], int num) {
@@ -29,15 +35,48 @@ public class DCMS_USER extends Thread { // 포트 8001
                     output1.write((String.valueOf(DATA.wm[getnum(data, 1) - 1][0]) + String.valueOf(DATA.wm[getnum(data, 1) - 1][1]) + String.valueOf(DATA.wm[getnum(data, 1) - 1][2]) + String.valueOf(DATA.wm[getnum(data, 1) - 1][3])).getBytes());
                 } else if (data[0] == '2') {                                                                  //샤워실일경우
                     output1.write((String.valueOf(DATA.sh[getnum(data, 1) - 1][0]) + String.valueOf(DATA.sh[getnum(data, 1) - 1][1])).getBytes());
-                } else if (data[0] == '3') {                                                                    //공지사항일경우
-                byte trash[]=new byte[5];
-                 for(int i=0;i<5;i++) {
-                     output1.write(DATA.notice[i].getBytes());                                               //DATA클래스에있는 notice 전송
-                     input1.read(trash);
-                     }
+                } else if (data[0] == '3') {                                                                   //공지사항일경우
+                    byte trash[] = new byte[3];
+                    for (int i = 0; i < 3; i++) {
+                        output1.write(DATA.notice[i].getBytes());                                               //DATA클래스에있는 notice 전송
+                        input1.read(trash);
+                    }
+                } else if (data[0] == '4') {
+                    Connection con = null;
+                    Statement sta = null;
+                    try {
+                        Class.forName("com.mysql.cj.jdbc.Driver");
+
+                        con = DriverManager.getConnection("jdbc:mysql://10.80.162.66:3306/dcms_db?serverTimezone=UTC", "test2", "1234");
+                        sta = con.createStatement();
+
+                        PreparedStatement pstmt = con.prepareStatement("select * from topic where id = "+ new String(data).substring(1,5)+";");
+                        ResultSet rs = pstmt.executeQuery();
+                        while(rs.next()) {
+                            System.out.print(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3) + " " + rs.getString(4) + "\n");
+                            byte trash[] = new byte[3];
+                            output1.write(rs.getString(1).getBytes());
+                            input1.read(trash);
+                            output1.write(rs.getString(2).getBytes());
+                            input1.read(trash);
+                            output1.write(rs.getString(4).getBytes());                                               //DATA클래스에있는 notice 전송
+                            input1.read(trash);
+                        }
+                        } catch (SQLException ex) {
+                        System.out.println("SQLException:" + ex);
+                    } catch (Exception ex) {
+                        System.out.println("Exception:" + ex);
+                    } finally {
+                        try {
+                            if (con != null) {
+                                con.close();
+                            }
+                        } catch (Exception e) {
+                        }
+                    }
                 }
-    }
-} catch (IOException e) {
+            }
+        } catch (IOException e) {
         }
 
     }
